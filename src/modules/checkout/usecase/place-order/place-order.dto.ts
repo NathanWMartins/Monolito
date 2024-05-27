562cf986a063e07dac345d581161fc98bebe1748
NathanWMartins <nathanwmartins@gmail.com>
2024-05-27 10:02:02 -0300

    Commit Error

---
+++ src/modules/checkout/usecase/place-order/place-order.dto.ts
@@ -0,0 +1,15 @@
+export interface PlaceOrderInputDTO {
+    clientId: string;
+    products:{
+        productId: string;
+    }[];
+}
+export interface PlaceOrderOutputDTO {
+    id: string;
+    invoiceId: string;
+    status: string;
+    total: number;
+    products: {
+        productId: string;
+    }[];
+}
